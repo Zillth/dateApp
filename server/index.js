a802680ba6bb04c565185148287efc0ffb59f7c6
Alan Flores <alanflores.r02@gmail.com>
2021-01-22 18:22:05 -0600

    Server configurated

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+const app = express()
+dotenv.config()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+//Mongo atlas connection url
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`)))
+    .catch(err => console.log(err))
+
+mongoose.set('useFindAndModify', false)
