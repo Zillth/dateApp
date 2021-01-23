095f9e2662e9d9b049026e28f79f4040bea571a9
Alan Flores <alanflores.r02@gmail.com>
2021-01-22 21:30:35 -0600

    users initialization, users actions done, users api done, constants created

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,3 +1,14 @@
 import axios from 'axios'
 
 const API = axios.create({ baseURL: 'http://localhost:5000' })
+
+// Users
+export const fetchUsers = () => API.get('/users')
+export const findUser = (userId) => API.get(`/users/${userId}`)
+export const createUser = (newUser) => API.post('/users', newUser)
+export const updateUser = (userId, userUpdated) => API.patch(`/users/${userId}`, userUpdated)
+export const deleteUser = (userId) => API.delete(`/users/${userId}`)
+
+// Auth
+export const signIn = (formData) => API.post('/users/signIn', formData)
+export const signUp = (formData) => API.post('/users/signUp', formData)
---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,2 @@
+// Users
+export const AUTH = 'AUTH'
